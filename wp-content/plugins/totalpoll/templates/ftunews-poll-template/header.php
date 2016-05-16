4ce08499cf427951de8bd4d1aad21c1a266dfa94
minhduc150496 <minhduc150496@gmail.com>
2016-05-16 21:52:23 +0700

    Sửa thể lệ Poll FTUCharm 2016

--- wp-content/plugins/totalpoll/templates/ftunews-poll-template/header.php
+++ wp-content/plugins/totalpoll/templates/ftunews-poll-template/header.php
@@ -1,4 +1,4 @@
-<?php if (!defined('ABSPATH')) exit; // Shhh    ?>
+﻿<?php if (!defined('ABSPATH')) exit; // Shhh    ?>
 <link href="<?php echo TP_ASSETS_URL ?>css/ftunews-poll.css" type="text/css" rel="stylesheet">
 
 <div class="poll-header">
@@ -32,13 +32,12 @@
         <?php if ($running):?>
         <h2>BÌNH CHỌN NỮ SINH DUYÊN DÁNG NHẤT ĐẠI HỌC NGOẠI THƯƠNG</h2>
         <div>
-            Thể lệ:<br>
-            Trải qua hai vòng thi sơ kết và bán kết cùng chuỗi hoạt động bên lề, cuộc thi đã tìm ra 16 gương mặt xinh
-            xắn nhất sẽ khoe sắc, tranh tài trong đêm chung kết tại nhà hát Bến Thành vào ngày 3/5 sắp tới. Tuy vậy ngay
-            từ bây giờ, teen đã có thể cùng tham gia bình chọn cho các thí sinh trên trang tin iOne.net. Hoạt động bình
-            chọn sẽ bắt đầu từ hôm nay 26/4 đến 17h ngày 3/5/2012 teen nhé. Thí sinh nhận được nhiều vote nhất từ chính
-            các bạn sẽ được trao danh hiệu Hoa khôi được yêu thích nhất trong đêm chung kết đấy! Duyên dáng Ngoại thương
-            2012 được trang tin iOne.net và trang ftunews.com của trường Ngoại Thương bảo trợ thông tin.
+            <p>Nhằm tìm ra và tôn vinh gương mặt Top 15 nhận được sự quan tâm và yêu thích nhất, FTUNEWS phối hợp cùng BTC cuộc thi Duyên dáng Ngoại thương – FTUCharm tổ chức cuộc thi bình chọn danh hiệu "Hoa khôi được yêu thích nhất".</p>
+            <p><b>Thời gian bình chọn:</b> Từ 17h ngày 18/5/2016 đến 17h ngày 28/5/2016</p>
+            <p><b>Cách thức bình chọn:</b> Bên phải mỗi tấm ảnh có hiển thị thông tin cá nhân và nút bình chọn. Người tham gia chỉ được bình chọn cho 1 thí sinh trong mỗi lần vote, hệ thống cho phép bình chọn 1 lần/ngày đối với mỗi người dùng. </p>
+            <p>Danh hiệu "Hoa khôi được yêu thích nhất" được trao hoàn toàn dựa trên kết quả bình chọn!</p>
+            <p>Kết quả bình chọn sẽ được công bố vào đêm Chung kết FTUCharm 2016, diễn ra tại Hội trường A5 Đại học Bách Khoa TP.HCM ngày 28.5.2016. </p>
+            <p>Lưu ý: Khi có bất cứ thắc mắc, khiếu nại nào xảy ra, BTC sẽ là người đưa ra quyết định cuối cùng.</p>
         </div>
         <?php endif;?>
     </div>
